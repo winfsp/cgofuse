baa4e69b4cfc58cdb50c1d14b7317dd5f5e963df
Bill Zissimopoulos <billziss@navimatics.com>
2017-04-20 01:45:33 -0700

    passthrough: add trace capability

--- examples/passthrough/passthrough.go
+++ examples/passthrough/passthrough.go
@@ -33,47 +33,55 @@ func errno(err error) int {
 	}
 }
 
-func (self *Ptfs) Statfs(path string, stat *fuse.Statfs_t) int {
+func (self *Ptfs) Statfs(path string, stat *fuse.Statfs_t) (errc int) {
+	defer Trace(path)(&errc, stat)
 	path = filepath.Join(self.root, path)
 	stgo := syscall.Statfs_t{}
-	errc := errno(syscall.Statfs(path, &stgo))
+	errc = errno(syscall.Statfs(path, &stgo))
 	copyFusestatfsFromGostatfs(stat, &stgo)
-	return errc
+	return
 }
 
-func (self *Ptfs) Mknod(path string, mode uint32, dev uint64) int {
+func (self *Ptfs) Mknod(path string, mode uint32, dev uint64) (errc int) {
+	defer Trace(path, mode, dev)(&errc)
 	path = filepath.Join(self.root, path)
 	return errno(syscall.Mknod(path, mode, int(dev)))
 }
 
-func (self *Ptfs) Mkdir(path string, mode uint32) int {
+func (self *Ptfs) Mkdir(path string, mode uint32) (errc int) {
+	defer Trace(path, mode)(&errc)
 	path = filepath.Join(self.root, path)
 	return errno(syscall.Mkdir(path, mode))
 }
 
-func (self *Ptfs) Unlink(path string) int {
+func (self *Ptfs) Unlink(path string) (errc int) {
+	defer Trace(path)(&errc)
 	path = filepath.Join(self.root, path)
 	return errno(syscall.Unlink(path))
 }
 
-func (self *Ptfs) Rmdir(path string) int {
+func (self *Ptfs) Rmdir(path string) (errc int) {
+	defer Trace(path)(&errc)
 	path = filepath.Join(self.root, path)
 	return errno(syscall.Rmdir(path))
 }
 
-func (self *Ptfs) Link(oldpath string, newpath string) int {
+func (self *Ptfs) Link(oldpath string, newpath string) (errc int) {
+	defer Trace(oldpath, newpath)(&errc)
 	oldpath = filepath.Join(self.root, oldpath)
 	newpath = filepath.Join(self.root, newpath)
 	return errno(syscall.Link(oldpath, newpath))
 }
 
-func (self *Ptfs) Symlink(target string, newpath string) int {
+func (self *Ptfs) Symlink(target string, newpath string) (errc int) {
+	defer Trace(target, newpath)(&errc)
 	target = filepath.Join(self.root, target)
 	newpath = filepath.Join(self.root, newpath)
 	return errno(syscall.Symlink(target, newpath))
 }
 
-func (self *Ptfs) Readlink(path string) (int, string) {
+func (self *Ptfs) Readlink(path string) (errc int, target string) {
+	defer Trace(path)(&errc, &target)
 	path = filepath.Join(self.root, path)
 	buff := [1024]byte{}
 	n, e := syscall.Readlink(path, buff[:])
@@ -83,23 +91,27 @@ func (self *Ptfs) Readlink(path string) (int, string) {
 	return 0, string(buff[:n])
 }
 
-func (self *Ptfs) Rename(oldpath string, newpath string) int {
+func (self *Ptfs) Rename(oldpath string, newpath string) (errc int) {
+	defer Trace(oldpath, newpath)(&errc)
 	oldpath = filepath.Join(self.root, oldpath)
 	newpath = filepath.Join(self.root, newpath)
 	return errno(syscall.Rename(oldpath, newpath))
 }
 
-func (self *Ptfs) Chmod(path string, mode uint32) int {
+func (self *Ptfs) Chmod(path string, mode uint32) (errc int) {
+	defer Trace(path, mode)(&errc)
 	path = filepath.Join(self.root, path)
 	return errno(syscall.Chmod(path, mode))
 }
 
-func (self *Ptfs) Chown(path string, uid uint32, gid uint32) int {
+func (self *Ptfs) Chown(path string, uid uint32, gid uint32) (errc int) {
+	defer Trace(path, uid, gid)(&errc)
 	path = filepath.Join(self.root, path)
 	return errno(syscall.Chown(path, int(uid), int(gid)))
 }
 
-func (self *Ptfs) Utimens(path string, tmsp1 []fuse.Timespec) int {
+func (self *Ptfs) Utimens(path string, tmsp1 []fuse.Timespec) (errc int) {
+	defer Trace(path, tmsp1)(&errc)
 	path = filepath.Join(self.root, path)
 	tmsp := [2]syscall.Timespec{}
 	tmsp[0].Sec, tmsp[0].Nsec = tmsp1[0].Sec, tmsp1[0].Nsec
@@ -107,12 +119,14 @@ func (self *Ptfs) Utimens(path string, tmsp1 []fuse.Timespec) int {
 	return errno(syscall.UtimesNano(path, tmsp[:]))
 }
 
-func (self *Ptfs) Access(path string, mask uint32) int {
+func (self *Ptfs) Access(path string, mask uint32) (errc int) {
+	defer Trace(path, mask)(&errc)
 	path = filepath.Join(self.root, path)
 	return errno(syscall.Access(path, mask))
 }
 
-func (self *Ptfs) Create(path string, mode uint32) (int, uint64) {
+func (self *Ptfs) Create(path string, mode uint32) (errc int, fh uint64) {
+	defer Trace(path, mode)(&errc, &fh)
 	path = filepath.Join(self.root, path)
 	f, e := syscall.Open(path, syscall.O_WRONLY|syscall.O_CREAT|syscall.O_TRUNC, mode)
 	if e != nil {
@@ -121,7 +135,8 @@ func (self *Ptfs) Create(path string, mode uint32) (int, uint64) {
 	return 0, uint64(f)
 }
 
-func (self *Ptfs) Open(path string, flags int) (int, uint64) {
+func (self *Ptfs) Open(path string, flags int) (errc int, fh uint64) {
+	defer Trace(path, flags)(&errc, &fh)
 	path = filepath.Join(self.root, path)
 	f, e := syscall.Open(path, flags, 0)
 	if e != nil {
@@ -131,6 +146,7 @@ func (self *Ptfs) Open(path string, flags int) (int, uint64) {
 }
 
 func (self *Ptfs) Getattr(path string, stat *fuse.Stat_t, fh uint64) (errc int) {
+	defer Trace(path, fh)(&errc, stat)
 	stgo := syscall.Stat_t{}
 	if ^uint64(0) == fh {
 		path = filepath.Join(self.root, path)
@@ -143,6 +159,7 @@ func (self *Ptfs) Getattr(path string, stat *fuse.Stat_t, fh uint64) (errc int) 
 }
 
 func (self *Ptfs) Truncate(path string, size int64, fh uint64) (errc int) {
+	defer Trace(path, size, fh)(&errc)
 	if ^uint64(0) == fh {
 		path = filepath.Join(self.root, path)
 		errc = errno(syscall.Truncate(path, size))
@@ -152,7 +169,8 @@ func (self *Ptfs) Truncate(path string, size int64, fh uint64) (errc int) {
 	return
 }
 
-func (self *Ptfs) Read(path string, buff []byte, ofst int64, fh uint64) int {
+func (self *Ptfs) Read(path string, buff []byte, ofst int64, fh uint64) (n int) {
+	defer Trace(path, buff, ofst, fh)(&n)
 	n, e := syscall.Pread(int(fh), buff, ofst)
 	if e != nil {
 		return errno(e)
@@ -160,7 +178,8 @@ func (self *Ptfs) Read(path string, buff []byte, ofst int64, fh uint64) int {
 	return n
 }
 
-func (self *Ptfs) Write(path string, buff []byte, ofst int64, fh uint64) int {
+func (self *Ptfs) Write(path string, buff []byte, ofst int64, fh uint64) (n int) {
+	defer Trace(path, buff, ofst, fh)(&n)
 	n, e := syscall.Pwrite(int(fh), buff, ofst)
 	if e != nil {
 		return errno(e)
@@ -168,15 +187,18 @@ func (self *Ptfs) Write(path string, buff []byte, ofst int64, fh uint64) int {
 	return n
 }
 
-func (self *Ptfs) Release(path string, fh uint64) int {
+func (self *Ptfs) Release(path string, fh uint64) (errc int) {
+	defer Trace(path, fh)(&errc)
 	return errno(syscall.Close(int(fh)))
 }
 
-func (self *Ptfs) Fsync(path string, datasync bool, fh uint64) int {
+func (self *Ptfs) Fsync(path string, datasync bool, fh uint64) (errc int) {
+	defer Trace(path, datasync, fh)(&errc)
 	return errno(syscall.Fsync(int(fh)))
 }
 
-func (self *Ptfs) Opendir(path string) (int, uint64) {
+func (self *Ptfs) Opendir(path string) (errc int, fh uint64) {
+	defer Trace(path)(&errc, &fh)
 	path = filepath.Join(self.root, path)
 	f, e := syscall.Open(path, syscall.O_RDONLY|syscall.O_DIRECTORY, 0)
 	if e != nil {
@@ -188,7 +210,8 @@ func (self *Ptfs) Opendir(path string) (int, uint64) {
 func (self *Ptfs) Readdir(path string,
 	fill func(name string, stat *fuse.Stat_t, ofst int64) bool,
 	ofst int64,
-	fh uint64) int {
+	fh uint64) (errc int) {
+	defer Trace(path, fill, ofst, fh)(&errc)
 	file, e := os.Open(path)
 	if e != nil {
 		return errno(e)
@@ -198,15 +221,16 @@ func (self *Ptfs) Readdir(path string,
 	if e != nil {
 		return errno(e)
 	}
-	for _, name := range(nams) {
-		if (!fill(name, nil, 0)) {
+	for _, name := range nams {
+		if !fill(name, nil, 0) {
 			break
 		}
 	}
 	return 0
 }
 
-func (self *Ptfs) Releasedir(path string, fh uint64) int {
+func (self *Ptfs) Releasedir(path string, fh uint64) (errc int) {
+	defer Trace(path, fh)(&errc)
 	return errno(syscall.Close(int(fh)))
 }
 
---
+++ examples/passthrough/trace.go
@@ -0,0 +1,89 @@
+/*
+ * trace.go
+ *
+ * Copyright 2017 Bill Zissimopoulos
+ */
+/*
+ * This file is part of Cgofuse.
+ *
+ * You can redistribute it and/or modify it under the terms of the GNU
+ * General Public License version 3 as published by the Free Software
+ * Foundation.
+ */
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"runtime"
+)
+
+func traceJoin(deref bool, vals []interface{}) string {
+	rslt := ""
+	for _, v := range vals {
+		if deref {
+			switch i := v.(type) {
+			case *bool:
+				rslt += fmt.Sprintf(", %#v", *i)
+			case *int:
+				rslt += fmt.Sprintf(", %#v", *i)
+			case *int8:
+				rslt += fmt.Sprintf(", %#v", *i)
+			case *int16:
+				rslt += fmt.Sprintf(", %#v", *i)
+			case *int32:
+				rslt += fmt.Sprintf(", %#v", *i)
+			case *int64:
+				rslt += fmt.Sprintf(", %#v", *i)
+			case *uint:
+				rslt += fmt.Sprintf(", %#v", *i)
+			case *uint8:
+				rslt += fmt.Sprintf(", %#v", *i)
+			case *uint16:
+				rslt += fmt.Sprintf(", %#v", *i)
+			case *uint32:
+				rslt += fmt.Sprintf(", %#v", *i)
+			case *uint64:
+				rslt += fmt.Sprintf(", %#v", *i)
+			case *uintptr:
+				rslt += fmt.Sprintf(", %#v", *i)
+			case *float32:
+				rslt += fmt.Sprintf(", %#v", *i)
+			case *float64:
+				rslt += fmt.Sprintf(", %#v", *i)
+			case *complex64:
+				rslt += fmt.Sprintf(", %#v", *i)
+			case *complex128:
+				rslt += fmt.Sprintf(", %#v", *i)
+			case *string:
+				rslt += fmt.Sprintf(", %#v", *i)
+			default:
+				rslt += fmt.Sprintf(", %#v", v)
+			}
+		} else {
+			rslt += fmt.Sprintf(", %#v", v)
+		}
+	}
+	if len(rslt) > 0 {
+		rslt = rslt[2:]
+	}
+	return rslt
+}
+
+func Trace(vals ...interface{}) func(vals ...interface{}) {
+	pc, _, _, ok := runtime.Caller(1)
+	name := "<UNKNOWN>"
+	if ok {
+		fn := runtime.FuncForPC(pc)
+		name = fn.Name()
+	}
+	args := traceJoin(false, vals)
+	return func(vals ...interface{}) {
+		f := "%v(%v) = (%v)"
+		if len(vals) == 1 {
+			f = "%v(%v) = %v"
+		}
+		log.Printf(f, name, args, traceJoin(true, vals))
+	}
+}
