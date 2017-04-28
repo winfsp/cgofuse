de984584a13c8399d016fcd6ed43997a021e8955
Bill Zissimopoulos <billziss@navimatics.com>
2017-04-28 12:48:26 -0700

    change license to MIT

--- examples/passthrough/port_linux.go
+++ examples/passthrough/port_linux.go
@@ -6,9 +6,8 @@
 /*
  * This file is part of Cgofuse.
  *
- * You can redistribute it and/or modify it under the terms of the GNU
- * General Public License version 3 as published by the Free Software
- * Foundation.
+ * It is licensed under the MIT license. The full license text can be found
+ * in the License.txt file at the root of this project.
  */
 
 // +build linux
