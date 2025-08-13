7d90b099247151dd4faf198cd53ad433f0ae9932
Bill Zissimopoulos <billziss@navimatics.com>
2025-08-13 14:06:01 +0300

    cgo: cosmetic fix

--- fuse/host_cgo.go
+++ fuse/host_cgo.go
@@ -173,7 +173,7 @@ static void *cgofuse_init_fuse(void)
 #if defined(__APPLE__)
 	// runtime path for bundled dylib in e.g. Awesome.app/Contents/Frameworks/libfuse.dylib
 	const char *dylib_path = getenv("CGOFUSE_LIBFUSE_PATH");
-	if(0 != dylib_path)
+	if (0 != dylib_path)
 		h = dlopen(dylib_path, RTLD_NOW);
 	if (0 == h)
 		h = dlopen("/usr/local/lib/libfuse.2.dylib", RTLD_NOW); // MacFUSE/OSXFuse >= v4
